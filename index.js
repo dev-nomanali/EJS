771690e6c055a4b266000036db37523bd4dfdf10
Noman Ali <devnomanali@gmail.com>
2025-08-25 22:07:26 +0530

    working on ejs files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
 
 app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -13,14 +16,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id:uuidv4(),
         username: "apnacollege",
         content: "i lovecodin"
     },
     {
+        id:uuidv4(),
         username: "Noman Ali",
         content: "Hard work is important ton achive success",
     },
     {
+        id:uuidv4(),
         username: "Naved Ali",
         content: "i got selected for my 1st internship!",
     },
@@ -37,8 +43,37 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content});
+    let id = uuidv4();
+    posts.push({ id, username, content});
     res.redirect("/posts")
+});
+
+app.get("/posts/:id", (req, res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) =>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port, () => {
